7b48eb116234908ade2a10df1fa13165960411b3
surendraCrossML <surendra.singh@crossml.com>
2026-02-26 23:53:47 +0530

    update Project dependencies and S3 Implmentation

--- app/core/config.py
+++ app/core/config.py
@@ -4,3 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 APP_NAME = os.getenv("APP_NAME")
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
+AWS_S3_BUCKET = os.getenv("AWS_S3_BUCKET")
